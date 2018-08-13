b89b251611b13f3281881b40808951d7aae71d73
Volchan <s.dlt@hotmail.fr>
2018-08-13 19:28:08 +0200

    session 5/53

--- app/src/main/java/fr/volchan/learnjava/Demo.java
+++ app/src/main/java/fr/volchan/learnjava/Demo.java
@@ -2,11 +2,24 @@ package fr.volchan.learnjava;
 
 public class Demo {
     public static void main(String[] args) {
-        Player player = new Player();
-        System.out.println(player.getHandleName());
-        player.setHandleName("Volchan");
-        System.out.println(player.getHandleName());
-        player.setHandleName("Vol");
-        System.out.println(player.getHandleName());
+        Player p1 = new Player();
+        System.out.println("Player One name: " + p1.getHandleName());
+        p1.setHandleName("Volchan");
+        System.out.println("Player One name: " + p1.getHandleName());
+        p1.setHandleName("Vol");
+        System.out.println("Player One name: " + p1.getHandleName());
+        System.out.println("Player One level: " + p1.getLevel());
+        System.out.println("Player One lives: " + p1.getLives());
+
+        Player p2 = new Player("Toto");
+        System.out.println("Player Two name: " + p2.getHandleName());
+        p2.setLives(5);
+        System.out.println("Player Two level: " + p2.getLevel());
+        System.out.println("Player Two lives: " + p2.getLives());
+
+        Player p3 = new Player("Tata", 9);
+        System.out.println("Player Three name: " + p3.getHandleName());
+        System.out.println("Player Three level: " + p3.getLevel());
+        System.out.println("Player Three lives: " + p3.getLives());
     }
 }
--- app/src/main/java/fr/volchan/learnjava/Player.java
+++ app/src/main/java/fr/volchan/learnjava/Player.java
@@ -7,9 +7,22 @@ public class Player {
     private int score;
 
     public Player() {
-        handleName = "Unknown player";
+        this("Unknown player");
+    }
+
+    public Player(String handle) {
+        this(handle, 1);
+        handleName = handle;
         lives = 3;
         level = 1;
+        score = 0;
+    }
+
+    public Player(String handle, int startingLevel) {
+        handleName = handle;
+        lives = 3;
+        level = startingLevel;
+
         score = 0;
     }
 
@@ -17,8 +30,32 @@ public class Player {
         return handleName;
     }
 
+    public int getLevel() {
+        return level;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
     public void setHandleName(String handle) {
         if (handle.length() < 4) return;
         handleName = handle;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
     }
 }
