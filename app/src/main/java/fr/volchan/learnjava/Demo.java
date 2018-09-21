58fc9b889d1e48b77ccbb4659760eac9272396ef
Volchan <s.dlt@hotmail.fr>
2018-09-21 20:24:16 +0200

    session 5/63

--- app/src/main/java/fr/volchan/learnjava/Demo.java
+++ app/src/main/java/fr/volchan/learnjava/Demo.java
@@ -1,5 +1,7 @@
 package fr.volchan.learnjava;
 
+import java.util.Random;
+
 public class Demo {
     public static void main(String[] args) {
 //        Player p1 = new Player();
@@ -62,22 +64,42 @@ public class Demo {
         vlad.takeDamage(8);
         vlad.showInfo();
 
-        VampireKing dracula = new VampireKing("Dracula");
-        dracula.showInfo();
-        dracula.takeDamage(40);
-        dracula.showInfo();
+        Random rand = new Random();
 
-        while (dracula.getLives() > 0) {
-            if (dracula.dodges()) {
-                continue;
+        for (int i=0; i<rand.nextInt(10)+1 ; i++) {
+            VampireKing dracula = new VampireKing("Dracula");
+            dracula.showInfo();
+            dracula.takeDamage(40);
+            dracula.showInfo();
+
+            while (dracula.getLives() > 0) {
+                if (dracula.dodges()) {
+                    continue;
+                }
+                if (dracula.runAway()) {
+                    System.out.println(dracula.getName() + " ran away");
+                    break;
+                } else {
+                    dracula.takeDamage(80);
+                    dracula.showInfo();
+                }
             }
-            if (dracula.runAway()) {
-                System.out.println(dracula.getName() + " ran away");
-                break;
-            } else {
-                dracula.takeDamage(12);
-                dracula.showInfo();
-            }
+            System.out.println("========================================");
         }
+
+//        Player conan = new Player("Conan");
+//        conan.pickUpLoot(new Loot("Invisibility", LootType.POTION, 4));
+//        conan.pickUpLoot(new Loot("Mithril", LootType.ARMOR, 183));
+//        conan.pickUpLoot(new Loot("Ring of speed", LootType.RING, 25));
+//        conan.pickUpLoot(new Loot("Red Potion", LootType.POTION, 2));
+////        conan.pickUpLoot(new Loot("Cursed Shield", LootType.ARMOR, -8));
+//        conan.pickUpLoot(new Loot("Brass Ring", LootType.RING, 1));
+//        conan.pickUpLoot(new Loot("Chain Mail", LootType.ARMOR, 4));
+//        conan.pickUpLoot(new Loot("Gold Ring", LootType.RING, 12));
+//        conan.pickUpLoot(new Loot("Health Potion", LootType.POTION, 3));
+//        conan.pickUpLoot(new Loot("Silver Ring", LootType.RING, 6));
+//        conan.showInventory();
+//
+//        System.out.println(conan.score());
     }
 }
--- app/src/main/java/fr/volchan/learnjava/Player.java
+++ app/src/main/java/fr/volchan/learnjava/Player.java
@@ -111,4 +111,15 @@ public class Player {
         }
         System.out.println("===============================");
     }
+
+    public int score() {
+        int total = 0;
+//        for (int i=0; i<inventory.size(); i++) {
+//            Loot currentLoot = inventory.get(i);
+        for (Loot currentLoot : inventory) {
+            System.out.println(currentLoot.getName() + " is worth " + currentLoot.getValue());
+            total = total + currentLoot.getValue();
+        }
+        return total;
+    }
 }
